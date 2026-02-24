5760a6c80a17e87dee8de39d79f57e51d783b446
drg25ii <93009091+drg25ii@users.noreply.github.com>
2026-02-24 17:12:33 +0200

    Update formatter.js

--- src/formatter.js
+++ src/formatter.js
@@ -10,10 +10,10 @@ function formatStream(stream, providerName) {
 
     // Format resolution
     let quality = stream.quality || '';
-    if (quality === '2160p') quality = 'EasyStreams 4K UHD';
-    else if (quality === '1440p') quality = 'EasyStreams QHD';
-    else if (quality === '1080p') quality = 'EasyStreams FHD';
-    else if (quality === '720p') quality = 'EasyStreams HD';
+    if (quality === '2160p') quality = 'EasyStreams  4K UHD';
+    else if (quality === '1440p') quality = 'EasyStreams  QHD';
+    else if (quality === '1080p') quality = 'EasyStreams  FHD';
+    else if (quality === '720p') quality = 'EasyStreams  HD';
     else if (quality === 'EasyStreams 576p' || quality === 'EasySreams 480p' || quality === 'EasyStreams 360p' || quality === 'EasyStreams 240p') quality = 'EasyStreams Low Quality';
     else if (!quality || quality.toLowerCase() === 'auto') quality = 'Unknown';
     
