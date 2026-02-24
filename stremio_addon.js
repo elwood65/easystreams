e86809c3ad9f9dd5bf0c7ae10f091c42cf185423
drg25ii <93009091+drg25ii@users.noreply.github.com>
2026-02-24 20:57:50 +0200

    Update stremio_addon.js

--- stremio_addon.js
+++ stremio_addon.js
@@ -25,7 +25,7 @@ const providers = {
 
 const builder = new addonBuilder({
     id: 'org.bestia.easystreams',
-    version: '1.0.0',
+    version: '1.0.43',
     name: 'Easy Streams',
     description: 'Italian Streams providers',
     catalogs: [],
@@ -454,4 +454,5 @@ const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => {
     console.log(`Stremio Addon running at http://localhost:${PORT}`);
 });
+
 
