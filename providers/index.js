ba468f9855ee53d8b497af50e1fd1fb8dcb018fd
nzo66 <233500038+realbestia1@users.noreply.github.com>
2026-05-24 18:20:17 +0200

    .

--- providers/index.js
+++ providers/index.js
@@ -13520,7 +13520,7 @@ function getStreams2(id, type, season, episode) {
         selectedProviders.push("streamingcommunity", "guardahd", "guardoserie", "altadefinizionestreaming", "vidxgo", "cinemacity");
       }
     } else if (normalizedType === "anime") {
-      selectedProviders.push("animeunity", "animeworld", "animesaturn", "guardoserie", "vidxgo");
+      selectedProviders.push("animeunity", "animeworld", "animesaturn", "guardoserie");
     } else if (normalizedType === "tv" || normalizedType === "series") {
       if (likelyAnime) {
         selectedProviders.push("animeunity", "animeworld", "animesaturn", "guardoserie");
